3f517f1820e2ff2a010482fae3c16d071dea971e
shenwl <shenweilun96@gmail.com>
2018-08-26 17:01:40 +0800

    icon组件，项目使用icon font Symbol

---
+++ src/icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg class="w-icon">
+        <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+        }
+    }
+}
+</script>
+
+<style>
+    .w-icon {
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
