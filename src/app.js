25c6091858083ac57dcacfb6cac1fd4de1bec55f
shenwl <shenweilun96@gmail.com>
2018-08-26 17:03:34 +0800

    app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Button from './button'
+import Icon from './icon'
+
+Vue.component('w-button', Button)
+Vue.component('w-icon', Icon)
+
+
+new Vue({
+    el: '#app',
+})
