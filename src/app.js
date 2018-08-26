1ecaeb7d16f80b9d0aa6f4cb282215cb21f6b5cb
shenwl <shenweilun96@gmail.com>
2018-08-26 17:21:46 +0800

    通过检测svg的order属性，测试iconPostion

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ new Vue({
     el: '#app',
 })
 
+
 // 单元测试
 import chai from 'chai'
 import spies from 'chai-spies'
@@ -19,6 +20,7 @@ chai.use(spies)
 const {expect} = chai
 
 {
+    // 测试Button的icon参数
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
         propsData: {
@@ -28,4 +30,23 @@ const {expect} = chai
     const useEle = vm.$el.querySelector('use')
     const href = useEle.getAttribute('xlink:href')
     expect(href).to.eq('#i-upload')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    // 测试Button的iconPostion参数
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'upload',
+            iconPosition: 'right',
+        }
+    }).$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    const order = window.getComputedStyle(svg).order
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
 }
