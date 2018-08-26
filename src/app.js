3b0bd0cac7a171a28085a95db756ecd1cf04fc99
shenwl <shenweilun96@gmail.com>
2018-08-26 17:11:55 +0800

    第一个单元测试：为Button组件的icon参数编写

--- src/app.js
+++ src/app.js
@@ -9,3 +9,23 @@ Vue.component('w-icon', Icon)
 new Vue({
     el: '#app',
 })
+
+// 单元测试
+import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
+
+const {expect} = chai
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'upload',
+        }
+    }).$mount()
+    const useEle = vm.$el.querySelector('use')
+    const href = useEle.getAttribute('xlink:href')
+    expect(href).to.eq('#i-upload')
+}
