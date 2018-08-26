6ad3aace91cd016fff5baa961576d20c5bc3022b
shenwl <shenweilun96@gmail.com>
2018-08-26 17:32:03 +0800

    添加button-group组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
+import ButtonGroup from './button-group'
 import Icon from './icon'
 
 Vue.component('w-button', Button)
+Vue.component('w-button-group', ButtonGroup)
 Vue.component('w-icon', Icon)
 
 
