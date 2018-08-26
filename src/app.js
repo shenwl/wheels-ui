52095beae87deb141fcc8f1077076a111da3e7ba
shenwl <shenweilun96@gmail.com>
2018-08-26 22:25:42 +0800

    单元测试try-catch

--- src/app.js
+++ src/app.js
@@ -21,50 +21,58 @@ chai.use(spies)
 
 const {expect} = chai
 
-{
-    // 测试Button的icon参数
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'upload',
-        }
-    }).$mount()
-    const useEle = vm.$el.querySelector('use')
-    const href = useEle.getAttribute('xlink:href')
-    expect(href).to.eq('#i-upload')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    // 测试Button的iconPostion参数
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'upload',
-            iconPosition: 'right',
-        }
-    }).$mount(div)
-    const svg = vm.$el.querySelector('svg')
-    const order = window.getComputedStyle(svg).order
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    // 测试Button的click事件
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'upload',
-        }
-    }).$mount()
-    const spy = chai.spy(() => {})
-    vm.$on('click', spy)
-    const button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-    vm.$el.remove()
-    vm.$destroy()
+try {
+    {
+        // 测试Button的icon参数
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'upload',
+            }
+        }).$mount()
+        const useEle = vm.$el.querySelector('use')
+        const href = useEle.getAttribute('xlink:href')
+        expect(href).to.eq('#i-upload')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        // 测试Button的iconPostion参数
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'upload',
+                iconPosition: 'right',
+            }
+        }).$mount(div)
+        const svg = vm.$el.querySelector('svg')
+        const order = window.getComputedStyle(svg).order
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        // 测试Button的click事件
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'upload',
+            }
+        }).$mount()
+        const spy = chai.spy(() => {})
+        vm.$on('click', spy)
+        const button = vm.$el
+        button.click()
+        expect(spy).to.have.been.called()
+        vm.$el.remove()
+        vm.$destroy()
+    }
+} catch(error) {
+    window.errors = [error]
+} finally {
+    window.errors && window.errors.forEach(error => {
+        console.error(error.message)
+    })
 }
