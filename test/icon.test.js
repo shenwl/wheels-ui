bb153342d8381bb5cd20f3cffd4ce90c9a68fbe9
shenwl <shenweilun96@gmail.com>
2018-08-27 20:39:46 +0800

    Icon 组件测试，存在 & props

---
+++ test/icon.test.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Icon from '../src/icon'
+
+const expect = chai.expect
+
+
+describe('Icon', () => {
+    it('存在', () => {
+        expect(Icon).to.be.exist
+    })
+    it('可以设置图标', () => {
+        const Constructor = Vue.extend(Icon)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        }).$mount()
+        const useEl = vm.$el.querySelector('use')
+        const href = useEl.getAttribute('xlink:href')
+        expect(href === '#i-settings').to.eq(true)
+    })
+})
