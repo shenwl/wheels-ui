48132cc2de5081c9a047dc6170a53488a6af5712
shenwl <shenweilun96@gmail.com>
2018-08-28 20:13:31 +0800

    更改icon 图标svg 引入方式

--- src/icon.vue
+++ src/icon.vue
@@ -5,18 +5,21 @@
 </template>
 
 <script>
-export default {
-    props: {
-        icon: {
-            type: String,
+    import './svg'
+
+    export default {
+        props: {
+            icon: {
+                type: String,
+            }
         }
     }
-}
 </script>
 
 <style>
     .w-icon {
-        width: 1em; height: 1em;
+        width: 1em;
+        height: 1em;
         vertical-align: -0.15em;
         fill: currentColor;
         overflow: hidden;
