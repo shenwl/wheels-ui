4d6290013b6fafff4851a61ca6eb96bba06e4253
shenwl <shenweilun96@gmail.com>
2018-08-28 21:40:52 +0800

    加入 Input组件

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './button-group'
 import Icon from './icon'
+import Input from './input'
 
 Vue.component('w-button', Button)
 Vue.component('w-button-group', ButtonGroup)
-Vue.component('w-icon', Icon)
+Vue.component('w-input', Input)
 
 
 new Vue({
