2664dd6fac76d1f032195b54a6d2f1bdbb198732
shenwl <shenweilun96@gmail.com>
2018-09-02 22:42:20 +0800

    flex 实现基本的栅格系统

--- src/col.vue
+++ src/col.vue
@@ -1,11 +1,34 @@
 <template>
+    <div class="w-col" :class="colClass">
 
+    </div>
 </template>
 
 <script>
-
+    export default {
+        name: "w-col",
+        computed: {
+            colClass() {
+                let {col} = this;
+                return [col && `col-${col}`];
+            }
+        },
+        props: {
+            col: {
+                type: [Number, String],
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
+    .w-col {
+        $prefix: col-;
 
+        @for $n from 1 through 24 {
+            &.#{$prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,17 @@
 <template>
-
+    <div class="w-row">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-
+    export default {
+        name: 'w-row',
+    }
 </script>
 
 <style lang="scss" scoped>
-
+    .w-row {
+        display: flex;
+    }
 </style>
