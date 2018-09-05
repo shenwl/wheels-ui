72e3511027724ffa7cc672ad037f33522abccc88
shenwl <shenweilun96@gmail.com>
2018-09-05 23:14:44 +0800

    row 和 col 测试

---
+++ test/grid.test.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Row from '../src/row'
+import Col from '../src/col'
+
+const expect = chai.expect
+
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row & Col', () => {
+    it('Row存在', () => {
+        expect(Row).to.exist
+    })
+
+    it('Col 存在', () => {
+        expect(Col).to.exist
+    })
+
+    it('接受 gutter 属性', (done) => {
+        Vue.component('w-row', Row)
+        Vue.component('w-col', Col)
+
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+            <w-row gutter="10">
+                <w-col span="12"></w-col>
+                <w-col span="12"></w-col>
+            </w-row>
+        `
+        document.body.appendChild(div)
+
+        const vm = new Vue({
+            el: div,
+        })
+
+        setTimeout(() => {
+            const cols = vm.$el.querySelectorAll('.w-col')
+            expect(getComputedStyle(cols[0]).paddingLeft).to.eq('5px')
+            expect(getComputedStyle(cols[1]).paddingRight).to.eq('5px')
+            done()
+        })
+    })
+})
