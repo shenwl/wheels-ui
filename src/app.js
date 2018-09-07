f7c2971e749104b2b060f7197418c59769cc131a
shenwl <shenweilun96@gmail.com>
2018-09-07 22:11:01 +0800

    alert, toast, confirm 三类弹窗

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@ import Button from './button'
 import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
-import Col from './Col'
+import Col from './col'
+import Toast from './toast'
+import Alert from './alert'
+import Confirm from './confirm'
 
 Vue.component('w-button', Button)
 Vue.component('w-button-group', ButtonGroup)
 Vue.component('w-input', Input)
 Vue.component('w-row', Row)
 Vue.component('w-col', Col)
+Vue.component('w-toast', Toast)
+Vue.component('w-alert', Alert)
+Vue.component('w-confirm', Confirm)
 
 
 new Vue({
