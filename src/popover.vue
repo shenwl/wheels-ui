e1998aa4444f54d32c4ebfd971e1a90cebaae918
shenwl <shenweilun96@gmail.com>
2018-10-28 22:10:45 +0800

    解决点击document时close两次bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-popover" @click.stop="handlePopoverClick">
+    <div class="w-popover" @click="handlePopoverClick" ref="popover">
         <div class="content-wrapper" ref="contentWrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
@@ -27,26 +27,40 @@
                 visible: false,
             }
         },
-
         methods: {
-            handlePopoverClick() {
-                this.visible = !this.visible
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            setContentPosition() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
 
-                        this.$refs.contentWrapper.style.left =  left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top =  top + window.scrollY + 'px'
-
-                        let eventHandler = (e) => {
-                            this.visible = false
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        this.$nextTick(() => {
-                            document.addEventListener('click', eventHandler)
-                        })
-                    })
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            addListenerToDocument() {
+                let eventHandler = (e) => {
+                    const refs = this.$refs
+                    const hasClose = !refs.contentWrapper
+                    const isTargetInContentWrapper = refs.contentWrapper && refs.contentWrapper.contains(e.target)
+                    const isTargetInPopover = refs.popover.contains(e.target) || refs.popover === e.target
+                    if (hasClose || isTargetInContentWrapper || isTargetInPopover) {return}
+                    this.visible = false
+                    document.removeEventListener('click', eventHandler)
+                }
+                this.$nextTick(() => {
+                    document.addEventListener('click', eventHandler)
+                })
+            },
+            onVisible() {
+                this.$nextTick(() => {
+                    this.setContentPosition()
+                    this.addListenerToDocument()
+                })
+            },
+            handlePopoverClick(e) {
+                if (this.$refs.triggerWrapper.contains(e.target)) {
+                    this.visible = !this.visible
+                    if (this.visible === true) {
+                        this.onVisible()
+                    }
                 }
             },
         }
@@ -59,8 +73,11 @@
         vertical-align: top;
         position: relative;
     }
+
     .content-wrapper {
         position: absolute;
         transform: translateY(-100%);
+        padding: 10px;
+        border: 1px solid #ccc;
     }
 </style>
