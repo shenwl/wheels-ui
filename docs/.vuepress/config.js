bf482898e99d22d35c9109663a641b7faf428d26
shenwl <shenweilun96@gmail.com>
2018-11-09 20:44:11 +0800

    使用vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'Wheels UI',
+    description: '一个好用的UI框架',
+    themeConfig: {
+        sidebar: [
+            {
+              title: '入门',
+              children: [
+                  '/install/',
+                  '/start/'
+              ]
+            },
+            {
+                title: '组件',
+                children: [
+                    '/components/button'
+                ]
+            }
+        ]
+    }
+}
